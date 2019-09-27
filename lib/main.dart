54fe314a739db3e84037970d0e60b874d1ab93fd
timmy <825125501@qq.com>
2019-09-27 11:41:29 +0800

    add Align widget to bottomRight

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,38 @@ class LayoutStarts extends StatelessWidget {
       children: <Widget>[
         CarDetailsAnimation(),
         CustomBottomSheet(),
+        RentButton(),
       ],
+    );
+  }
+}
+
+class RentButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: SizedBox(
+        width: 200,
+        child: FlatButton(
+          onPressed: () {},
+          child: Text(
+            "Rent Car",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                letterSpacing: 1.5,
+                fontFamily: "arial"),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15),
+            ),
+          ),
+          color: Colors.black,
+          padding: EdgeInsets.all(25),
+        ),
+      ),
     );
   }
 }
